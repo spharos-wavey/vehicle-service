fcef6c9e28ade360e04162ef6ff05bb4dd54a004
minji <kmj99a@gmail.com>
2023-05-09 10:43:15 +0900

    [Create] 차량 예약 에러 코드

--- src/main/java/xyz/wavey/vehicleservice/base/exception/ErrorCode.java
+++ src/main/java/xyz/wavey/vehicleservice/base/exception/ErrorCode.java
@@ -9,7 +9,8 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    NOT_FOUND_RENTAL("등록된 차량 정보가 존재하지 않습니다.", NOT_FOUND, 404)
+    NOT_FOUND_VEHICLE("등록된 차량 정보가 존재하지 않습니다.", NOT_FOUND, 404),
+    NOT_FOUND_BOOKLIST("등록된 예약 정보가 존재하지 않습니다.",NOT_FOUND,404)
     ;
     private final String message;
     private final HttpStatus httpStatus;

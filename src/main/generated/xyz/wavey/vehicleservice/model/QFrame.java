36452d7e2cc43ba6952f8c6cdaf8fe8d3160093e
ShinHyeonchae <72970253+scsok13@users.noreply.github.com>
2023-05-11 14:43:57 +0900

    [Create] QueryDSL 사용 및 빌드해서 Q클래스가 추가하였습니다.

---
+++ src/main/generated/xyz/wavey/vehicleservice/model/QFrame.java
@@ -0,0 +1,69 @@
+package xyz.wavey.vehicleservice.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QFrame is a Querydsl query type for Frame
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QFrame extends EntityPathBase<Frame> {
+
+    private static final long serialVersionUID = -1204257370L;
+
+    public static final QFrame frame = new QFrame("frame");
+
+    public final xyz.wavey.vehicleservice.base.QBaseTimeEntity _super = new xyz.wavey.vehicleservice.base.QBaseTimeEntity(this);
+
+    public final StringPath appearance = createString("appearance");
+
+    public final StringPath capacity = createString("capacity");
+
+    public final StringPath carType = createString("carType");
+
+    public final StringPath color = createString("color");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final NumberPath<Integer> defaultPrice = createNumber("defaultPrice", Integer.class);
+
+    public final NumberPath<Integer> distancePrice = createNumber("distancePrice", Integer.class);
+
+    public final BooleanPath foreignCar = createBoolean("foreignCar");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath image = createString("image");
+
+    public final StringPath maker = createString("maker");
+
+    public final StringPath manuel = createString("manuel");
+
+    public final StringPath name = createString("name");
+
+    public final BooleanPath recommend = createBoolean("recommend");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QFrame(String variable) {
+        super(Frame.class, forVariable(variable));
+    }
+
+    public QFrame(Path<? extends Frame> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QFrame(PathMetadata metadata) {
+        super(Frame.class, metadata);
+    }
+
+}
+
---
+++ src/main/generated/xyz/wavey/vehicleservice/model/QReview.java
@@ -0,0 +1,53 @@
+package xyz.wavey.vehicleservice.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = 1654893119L;
+
+    public static final QReview review = new QReview("review");
+
+    public final xyz.wavey.vehicleservice.base.QBaseTimeEntity _super = new xyz.wavey.vehicleservice.base.QBaseTimeEntity(this);
+
+    public final StringPath content = createString("content");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createDate = _super.createDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath nickName = createString("nickName");
+
+    public final StringPath profile = createString("profile");
+
+    public final EnumPath<Type> type = createEnum("type", Type.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updateDate = _super.updateDate;
+
+    public QReview(String variable) {
+        super(Review.class, forVariable(variable));
+    }
+
+    public QReview(Path<? extends Review> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReview(PathMetadata metadata) {
+        super(Review.class, metadata);
+    }
+
+}
+

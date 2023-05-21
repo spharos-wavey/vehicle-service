cc1062e52869710f6d17eac26e00f89f3fcdd3a5
Ji-Wook, Kim <90381800+N1ghtsky0@users.noreply.github.com>
2023-05-21 18:16:09 +0900

    [Update] 빌리타존내의 차량 목록 api 적용

--- src/main/java/xyz/wavey/vehicleservice/controller/VehicleController.java
+++ src/main/java/xyz/wavey/vehicleservice/controller/VehicleController.java
@@ -7,10 +7,6 @@ import org.springframework.web.bind.annotation.*;
 import xyz.wavey.vehicleservice.service.VehicleService;
 import xyz.wavey.vehicleservice.vo.RequestVehicle;
 import xyz.wavey.vehicleservice.vo.ResponseGetVehicle;
-import xyz.wavey.vehicleservice.vo.ResponseGetVehicleInBillitaZone;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping("/vehicle")
@@ -29,38 +25,15 @@ public class VehicleController {
         return vehicleService.getVehicle(id);
     }
 
-    @GetMapping()
-    public ResponseEntity<Object> getVehicleInBillitaZone(@RequestParam("billitazone") String id) {
-        List<ResponseGetVehicleInBillitaZone> responseGetVehicleInBillitaZoneList = new ArrayList<>();
-        responseGetVehicleInBillitaZoneList.add(ResponseGetVehicleInBillitaZone.builder()
-                        .vehicleModel("테스트 모델 1")
-                        .vehicleId(1L)
-                        .canBook(true)
-                        .defaultPrice(27000)
-                        .vehicleImage("https://storage.googleapis.com/bucket_billita_vehicle/ac6050b590c42d7f/Audi/%EC%95%84%EC%9A%B0%EB%94%94_%EC%9D%B4%ED%8A%B8%EB%A1%A0%20GT_%EC%8A%A4%EC%A6%88%EC%B9%B4%20%EA%B7%B8%EB%A0%88%EC%9D%B4%20%EB%A9%94%ED%83%88%EB%A6%AD.png")
-                        .distancePrice(100)
-                        .currentCharge(77)
-                .build());
-        responseGetVehicleInBillitaZoneList.add(ResponseGetVehicleInBillitaZone.builder()
-                .vehicleModel("테스트 모델 2")
-                .vehicleId(2L)
-                .canBook(true)
-                .defaultPrice(24000)
-                .vehicleImage("https://storage.googleapis.com/bucket_billita_vehicle/ac6050b590c42d7f/Audi/%EC%95%84%EC%9A%B0%EB%94%94_%EC%9D%B4%ED%8A%B8%EB%A1%A0%20GT_%EC%8A%A4%EC%A6%88%EC%B9%B4%20%EA%B7%B8%EB%A0%88%EC%9D%B4%20%EB%A9%94%ED%83%88%EB%A6%AD.png")
-                .distancePrice(100)
-                .currentCharge(60)
-                .build());
-        responseGetVehicleInBillitaZoneList.add(ResponseGetVehicleInBillitaZone.builder()
-                .vehicleModel("테스트 모델 3")
-                .vehicleId(3L)
-                .canBook(false)
-                .defaultPrice(20000)
-                .vehicleImage("https://storage.googleapis.com/bucket_billita_vehicle/ac6050b590c42d7f/Audi/%EC%95%84%EC%9A%B0%EB%94%94_%EC%9D%B4%ED%8A%B8%EB%A1%A0%20GT_%EC%8A%A4%EC%A6%88%EC%B9%B4%20%EA%B7%B8%EB%A0%88%EC%9D%B4%20%EB%A9%94%ED%83%88%EB%A6%AD.png")
-                .distancePrice(100)
-                .currentCharge(77)
-                .build());
+    @GetMapping("/billitazone")
+    public ResponseEntity<Object> getVehicleInBillitaZone(
+            @RequestParam("id") String id,
+            @RequestParam("sDate") String sDate,
+            @RequestParam("eDate") String eDate) {
 
-        return ResponseEntity.status(HttpStatus.OK).body(responseGetVehicleInBillitaZoneList);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(vehicleService.getVehicleInBillitaZone(Long.valueOf(id), sDate, eDate));
     }
 
 }

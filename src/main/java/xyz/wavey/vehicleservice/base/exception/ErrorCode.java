e565cb52b4335397cc94cf03c766946422469e7d
Ji-Wook, Kim <20korea01@gmail.com>
2023-05-22 11:49:40 +0900

    [Create] maker 에러코드

--- src/main/java/xyz/wavey/vehicleservice/base/exception/ErrorCode.java
+++ src/main/java/xyz/wavey/vehicleservice/base/exception/ErrorCode.java
@@ -14,6 +14,7 @@ public enum ErrorCode {
     NOT_FOUND_BILLITAZONE("등록된 빌리타존 정보가 존재하지 않습니다.", NOT_FOUND, 404),
     NOT_FOUND_FRAME("등록된 프레임 정보가 존재하지 않습니다.", NOT_FOUND, 404),
     NOT_FOUND_REVIEW("등록된 후기가 존재하지 않습니다.", NOT_FOUND, 404),
+    NOT_FOUND_MAKER("등록된 메이커가 존재하지 않습니다.", NOT_FOUND, 404),
 
     BAD_REQUEST_DATEFORMAT("날짜 형식이 잘못되었습니다.", BAD_REQUEST, 400)
     ;

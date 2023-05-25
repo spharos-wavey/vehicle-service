8459c5269c73d95ff486b9dd21f93a3537bedbfe
ShinHyeonchae <72970253+scsok13@users.noreply.github.com>
2023-05-25 12:05:52 +0900

    [Update] BillitaZone관련 ResponseEntity controller단으로 수정 하였습니다.

--- src/main/java/xyz/wavey/vehicleservice/controller/BillitaZoneController.java
+++ src/main/java/xyz/wavey/vehicleservice/controller/BillitaZoneController.java
@@ -5,52 +5,67 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import xyz.wavey.vehicleservice.model.BillitaZone;
 import xyz.wavey.vehicleservice.service.BillitaZoneService;
 import xyz.wavey.vehicleservice.vo.RequestBillitaZone;
-import xyz.wavey.vehicleservice.vo.ResponseBillitaZone;
+import xyz.wavey.vehicleservice.vo.ResponseGetAllBillitaZone;
 import xyz.wavey.vehicleservice.vo.ResponseGetNowBillita;
 
 @RestController
 @RequestMapping("/billitazone")
 @RequiredArgsConstructor
 public class BillitaZoneController {
+
     private final BillitaZoneService billitaZoneService;
 
     @PostMapping()
-    public ResponseEntity<Object> addBillitaZone(@RequestBody RequestBillitaZone requestBillitaZone) {
-        return billitaZoneService.addBillitaZone(requestBillitaZone);
+    public ResponseEntity<Object> addBillitaZone(
+        @RequestBody RequestBillitaZone requestBillitaZone) {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(billitaZoneService.addBillitaZone(requestBillitaZone));
     }
 
     @GetMapping()
     public ResponseEntity<Object> getAllBillitaZone() {
-        return billitaZoneService.getAllBillitaZone();
+        List<ResponseGetAllBillitaZone> billitaZoneList = billitaZoneService.getAllBillitaZone();
+
+        if (billitaZoneList.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        } else {
+            return ResponseEntity.status(HttpStatus.OK)
+                .body(billitaZoneList);
+        }
     }
 
     @GetMapping("/{id}")
-    public ResponseBillitaZone getBillitaZone(@PathVariable Long id) {
-        return billitaZoneService.getBillitaZone(id);
+    public ResponseEntity<Object> getBillitaZone(@PathVariable Long id) {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(billitaZoneService.getBillitaZone(id));
     }
 
     @GetMapping("/filter")
     public ResponseEntity<Object> vehicleTimeFilter(
-            @RequestParam("sDate") String startDate,
-            @RequestParam("eDate") String endDate,
-            @RequestParam("lat") String lat,
-            @RequestParam("lng") String lng) {
-        return billitaZoneService.timeFilter(startDate, endDate, Double.parseDouble(lat), Double.parseDouble(lng));
+        @RequestParam("sDate") String startDate,
+        @RequestParam("eDate") String endDate,
+        @RequestParam("lat") String lat,
+        @RequestParam("lng") String lng) {
+        return billitaZoneService.timeFilter(startDate, endDate, Double.parseDouble(lat),
+            Double.parseDouble(lng));
     }
 
     @GetMapping("/now")
     public ResponseEntity<Object> getNowBillita(
-            @RequestParam("lat") String lat,
-            @RequestParam("lng") String lng) {
+        @RequestParam("lat") String lat,
+        @RequestParam("lng") String lng) {
 
-        List<ResponseGetNowBillita> responseGetNowBillitaList = billitaZoneService.getNowBillita(Double.parseDouble(lat), Double.parseDouble(lng));
+        List<ResponseGetNowBillita> responseGetNowBillitaList = billitaZoneService.getNowBillita(
+            Double.parseDouble(lat), Double.parseDouble(lng));
 
-        if(responseGetNowBillitaList.isEmpty()){
+        if (responseGetNowBillitaList.isEmpty()) {
             return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
-        }
-        else {
+        } else {
             return ResponseEntity.status(HttpStatus.OK)
                 .body(responseGetNowBillitaList);
         }
--- src/main/java/xyz/wavey/vehicleservice/service/BillitaZoneService.java
+++ src/main/java/xyz/wavey/vehicleservice/service/BillitaZoneService.java
@@ -4,17 +4,18 @@ import org.springframework.http.ResponseEntity;
 import xyz.wavey.vehicleservice.model.BillitaZone;
 import xyz.wavey.vehicleservice.vo.RequestBillitaZone;
 import xyz.wavey.vehicleservice.vo.ResponseBillitaZone;
+import xyz.wavey.vehicleservice.vo.ResponseGetAllBillitaZone;
 import xyz.wavey.vehicleservice.vo.ResponseGetNowBillita;
 
 import java.util.List;
 
 public interface BillitaZoneService {
 
-    ResponseEntity<Object> addBillitaZone(RequestBillitaZone requestBillitaZone);
+    BillitaZone addBillitaZone(RequestBillitaZone requestBillitaZone);
 
     ResponseBillitaZone getBillitaZone(Long id);
 
-    ResponseEntity<Object> getAllBillitaZone();
+    List<ResponseGetAllBillitaZone> getAllBillitaZone();
 
     // 예약 시간과 사용자의 위경도가 주어졌을 때 빌리타존별 이용가능한 차량을 조회하는 서비스
     ResponseEntity<Object> timeFilter(String sDate, String eDate, double lat, double lng);

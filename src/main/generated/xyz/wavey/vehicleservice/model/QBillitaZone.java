990cd6296673e8b862421e3a6c7050946e25025a
Ji-Wook, Kim <20korea01@gmail.com>
2023-05-30 09:22:16 +0900

    [Create] 지역명 컬럼 추가

--- src/main/generated/xyz/wavey/vehicleservice/model/QBillitaZone.java
+++ src/main/generated/xyz/wavey/vehicleservice/model/QBillitaZone.java
@@ -27,6 +27,8 @@ public class QBillitaZone extends EntityPathBase<BillitaZone> {
 
     public final StringPath name = createString("name");
 
+    public final StringPath regionName = createString("regionName");
+
     public final StringPath zoneAddress = createString("zoneAddress");
 
     public QBillitaZone(String variable) {

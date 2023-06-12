5eec7c9a6d3e5e160b5e9907aaecaabf5cb5d16a
Ji-Wook, Kim <20korea01@gmail.com>
2023-06-12 15:07:05 +0900

    [Update] 예약 가능차량확인 api

--- src/main/java/xyz/wavey/vehicleservice/repository/BookListRepo.java
+++ src/main/java/xyz/wavey/vehicleservice/repository/BookListRepo.java
@@ -13,6 +13,6 @@ public interface BookListRepo extends JpaRepository<BookList, Long> {
     @Query(value = "select b from BookList as b where b.vehicle.id = :id and b.endDate > :startDate and b.startDate < :endDate")
     List<BookList> timeFilter(@Param("id") Long id, @Param("startDate") LocalDateTime startDate, @Param("endDate") LocalDateTime endDate);
 
-    Boolean existsByIdAndEndDateIsAfterAndStartDateIsBefore(Long id, LocalDateTime sDate, LocalDateTime eDate);
+    Boolean existsByVehicleIdAndEndDateIsAfterAndStartDateIsBefore(Long id, LocalDateTime sDate, LocalDateTime eDate);
 
 }
